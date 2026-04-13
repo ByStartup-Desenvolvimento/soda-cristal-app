83411f226acab50d366d12b915d145122c690e7d
Ivan Martins <ivan.martins@bystartup.com.br>
2026-04-13 13:37:19 -0300

    fix(android): use ip for routes

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,5 +1,11 @@
+import { Capacitor } from '@capacitor/core';
+
+const API_BASE_URL = Capacitor.isNativePlatform()
+    ? 'https://178.16.140.5/api'
+    : '/api';
+
 export const API_CONFIG = {
-    BASE_URL: '/api',
+    BASE_URL: API_BASE_URL,
     APP_VERSION: '30.19.2',
     HEADERS: {
         CONTENT_TYPE: 'application/json',
